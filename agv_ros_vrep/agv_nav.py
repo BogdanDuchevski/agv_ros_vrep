726e385d9c63f50b891e604301dd362007b36497
BogdanDuchevski <bd307@live.mdx.ac.uk>
2018-08-04 22:48:19 +0100

    Update Vrep-Ros Navigation
    This Commit adds a timer to the Sensor trigger, increasing steering time and improving obstacle avoidance functionality.
    
     - Add Timer functionality to Sensor trigger
     - Increase Kp (steering gain value)

--- agv_ros_vrep/agv_nav.py
+++ agv_ros_vrep/agv_nav.py
@@ -13,16 +13,19 @@ from std_msgs.msg import Float32
 from std_msgs.msg import Bool
 
 # - INIT
+ck =-False
 save_spd = None
 #pub_sp = rospy.Publisher('sensorTrigger', Float32, queue_size=10)
 nav_mode = False
 sim_in = False
 sensor = None
+start_time = None
+prob_time = None
 def callback_sens(data):
     global sensor
     if data > 0:
     #if data > 0:
-        print 'true',data.data
+        #print 'true',data.data
         sensor = data.data
         #pub_sp.publish(save_spd)
 
@@ -30,27 +33,43 @@ def callback_spd(data):
     global save_spd
     if data > 0:
     #if data > 0:
-        print 'true',data.data
+        #print 'true',data.data
         save_spd = data.data
         #pub_sp.publish(save_spd)
 
 def callback_sim(data):
     global save_spd
     global sensor
+    global ck
+    global start_time
+    global prob_time
     pub = rospy.Publisher('leftMotorSpeed', Float32, queue_size=10)
     pub2 = rospy.Publisher('rightMotorSpeed', Float32, queue_size=10)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
         if sensor==True:
-            steer=-1#/sensor_loc[min_ind]
-        else:
+            if start_time == None:
+                start_time = time.time()
+                prob_time = start_time
+            #else:
+        if start_time is not None:
+            if prob_time<start_time+3:
+                ck = True
+            else:
+                ck = False
+                start_time = None
+            prob_time = time.time()
+
+        if sensor == False and ck == False:
             steer=0
+        else:
+            steer=-1#/sensor_loc[min_ind]'
         kp=1.5	#steering gain
         vl=save_spd+kp*steer
         vr=save_spd-kp*steer
         pub.publish(float(vl))
         pub2.publish(float(vr))
-        #rate.sleep()
+        rate.sleep()
 
 def listener():
     rospy.init_node('rosBubbleRob2', anonymous=True)
